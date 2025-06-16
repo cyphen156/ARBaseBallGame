276dd71abb66b748883220acb3a1230602009538
cyphen156 <yungtaekoh@gmail.com>
2025-06-16 17:55:11 +0900

    [Add] Types.cs
    게임 로직에 사용될 Enum타입 추가

---
+++ Assets/ARBaseball/Scripts/Managers/GameManager.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using UnityEngine;
+
+/// <summary>
+/// 게임의 전반적인 상태를 관리합니다.
+/// 사용자의 입력에 따라 게임의 진행 상태를 업데이트하고, 게임 오브젝트를 관리합니다.
+/// AI와의 상호작용을 처리하며, 게임의 시작과 종료를 관리합니다.
+/// </summary>
+
+public class GameManager : MonoBehaviour
+{
+    [Header("GameState")]
+    public static GameManager Instance { get; private set; }
+    [SerializeField] private GameState currentGameState;  // 현재 게임 상태
+
+    [SerializeField] private PlayMode currentPlayMode;  // 플레이 모드 선택 여부
+
+    //[Header("PlayerControll")]
+    //public PlayerController PlayerController { get; private set; }
+
+    //[Header("AIControll")]
+    //public AIController AIController { get; private set; }
+
+
+    //[Header("UIControll")]
+    //public UIController UIController { get; private set; }
+
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private IEnumerator Start()
+    {
+        yield return StartCoroutine(InitializeGame());
+    }
+
+    private void Update()
+    {
+        // 게임 상태에 따라 업데이트 로직을 처리합니다.
+        switch (currentGameState)
+        {
+            case GameState.None:
+                // 초기 상태, 게임이 시작되지 않았습니다.
+                return;
+            case GameState.Ready:
+                // 플레이어가 게임을 시작할 준비가 되었습니다.
+                // 예: UI를 통해 플레이어가 게임을 시작할 수 있도록 안내합니다.
+                break;
+            case GameState.Play:
+                // 게임이 진행 중입니다.
+                // 예: 플레이어와 AI의 턴을 처리합니다.
+                break;
+            case GameState.End:
+                // 게임이 종료되었습니다.
+                // 예: 승패를 결정하고, 결과를 표시합니다.
+                break;
+            default:
+                break;
+        }
+    }
+
+    /// <summary>
+    /// // AR을 통해 플레이어와 AI의 위치를 설정하고, 게임 오브젝트를 초기화합니다.
+    /// 위치 설정이 완료되면 플레이어는 플레이 모드를 선택할 수 있습니다.
+    /// 플레이 모드가 선택되면 게임 상태를 Ready로 변경합니다.
+    /// </summary>
+    private IEnumerator InitializeGame()
+    {
+        // 게임 초기화 로직을 여기에 작성합니다.
+        // 예: 플레이어와 AI 컨트롤러 초기화, UI 설정 등
+        Debug.Log("게임 초기화 중...");
+        ChangeGameState(GameState.Initializing);
+        ChangePlayMode(PlayMode.None); // 초기 플레이 모드 설정
+
+        // 로직 1
+        // AR을 통해 플레이어와 AI의 위치를 설정하는 로직을 여기에 작성합니다.
+        //yield return new WaitUntil(() => arIsReady);
+
+        // 로직 2
+        // UI를 통해 플레이 모드를 선택하도록 안내합니다.
+        yield return new WaitUntil(() => currentPlayMode != PlayMode.None);
+
+        ChangeGameState(GameState.Ready);
+        Debug.Log("게임 플레이 준비 완료. 현재 상태: " + currentGameState);
+    }
+
+    private void ChangeGameState(GameState newGameState)
+    {
+        if (currentGameState == newGameState)
+        {
+            return; // 현재 상태와 동일하면 변경하지 않음
+        }
+        currentGameState = newGameState;
+        Debug.Log("게임 상태가 변경되었습니다: " + currentGameState);
+    }
+
+    private void ChangePlayMode(PlayMode newMode)
+    {
+        if (newMode == currentPlayMode)
+        {
+            return;
+        }
+
+        currentPlayMode = newMode;
+        Debug.Log("플레이 모드가 변경되었습니다: " + currentPlayMode);
+    }
+}
---
+++ Assets/ARBaseball/Scripts/types.cs
@@ -0,0 +1,81 @@
+﻿/// <summary>
+/// 게임 상태를 정의하는 열거형입니다.
+/// None, Initializing, Ready, Play, End
+/// </summary>
+public enum GameState
+{
+    None,           // 비정상적인 상태
+    Initializing,   // 게임이 시작되지 않은 상태, 초기화가 필요한 상태
+    Ready,          // 플레이어가 게임 플레이를 위해 모든 준비 상태가 완료된 상태
+    Play,           // 게임이 실제로 진행 중인 상태
+    End             // 게임이 종료된 상태, 승패가 결정되었거나 플레이어가 게임을 종료한 상태
+}
+
+/// <summary>
+/// 플레이 모드를 정의하는 열거형입니다.
+/// None, PitcherMode, BatterMode
+/// </summary>
+public enum PlayMode
+{
+    None,
+    PitcherMode,
+    BatterMode
+}
+
+/// <summary>
+/// 멀티 플레이 모드를 정의하는 열거형입니다.
+/// SinglePlayMode, MultiPlayMode
+/// </summary>
+public enum GameMode
+{
+    SinglePlayMode,
+    MultiPlayMode
+}
+
+/// <summary>
+/// 게임 결과를 정의하는 열거형입니다.
+/// None, PlayerWin, AIWin
+/// </summary>
+public enum GameResult
+{
+    None,
+    PlayerWin,
+    AIWin,
+}
+
+/// <summary>
+/// 각 턴의 결과를 정의하는 열거형입니다.
+/// None, Strike, Ball, Foul, Hit, HomeRun, Walk
+/// </summary>
+public enum TurnResult
+{
+    None,        // 턴 결과가 없는 상태
+    Strike,      // 스트라이크
+    Ball,        // 볼
+    Foul,        // 파울
+    Hit,         // 히트
+    HomeRun,     // 홈런
+    Walk         // 볼넷
+}
+
+/// <summary>
+/// 한 사람이 타석에 서 있는 동안 
+/// 행동의 결과를 정의하는 열거형입니다.
+/// None, Ready, Ongoing, Out, Complete 
+/// </summary>
+public enum TurnSessionResult
+{
+    None,        // 비정상
+    Ready,       // 타석에 진입했지만 아직 투구 시작 전
+    Ongoing,     // 투구 중, 세션 진행 중
+    Out,         // 삼진 등으로 타석 종료됨
+    Complete     // 히트, 홈런, 볼넷 등으로 종료됨
+}
+
+/// <summary>
+/// 타입 정의를 위한 클래스입니다.
+/// </summary>
+public class types
+{
+    // 이 클래스는 타입 정의를 위한 용도로 사용됩니다.
+}
